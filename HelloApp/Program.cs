52ea96134c4735f070491cf3cfd8da132d483768
missPhishingLady <caramelNicky0925@gmail.com>
2023-03-16 16:09:27 +0900

    a

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -11,6 +11,7 @@ namespace HelloApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world");
+            Console.WriteLine("Hello, World");
         }
     }
 }
